cefb8d9dca631fb175cec8d106c3ccf48d71b0d4
Artur <maslovskij.artur@gmail.com>
2017-02-05 22:28:13 +0200

    first commit

---
+++ Transport/App_Start/BundleConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Optimization;
+
+namespace Transport
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Vendor scripts
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/vendors/jquery-2.1.1.min.js"));
+
+            // jQuery Validation
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            "~/Scripts/vendors/jquery.validate.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/vendors/bootstrap.min.js"));
+
+            // Inspinia script
+            bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
+                      "~/Scripts/app/Transport.js"));
+
+            // SlimScroll
+            bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
+                      "~/Scripts/plugins/slimScroll/jquery.slimscroll.min.js"));
+
+            // jQuery plugins
+            bundles.Add(new ScriptBundle("~/plugins/metsiMenu").Include(
+                      "~/Scripts/plugins/metisMenu/metisMenu.min.js"));
+
+            bundles.Add(new ScriptBundle("~/plugins/pace").Include(
+                      "~/Scripts/plugins/pace/pace.min.js"));
+
+            // CSS style (bootstrap/inspinia)
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/animate.css",
+                      "~/Content/style.css"));
+
+            // Font Awesome icons
+            bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+                      "~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+        }
+    }
+}
